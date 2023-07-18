53a563e7f129f2ca715a605d9bc996fca6c4594d
mhfarhann <farhan.lagi.tidur@gmail.com>
2023-07-18 21:20:11 +0700

    add conversation procedure

--- src/server/api/routers/message.ts
+++ src/server/api/routers/message.ts
@@ -2,23 +2,23 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc";
 
 export const messageRouter = createTRPCRouter({
-  sendMessage: protectedProcedure
-  .input(z.object({message: z.string(), productId: z.string(),
-    toUser: z.string()
-  }))
-  .mutation(async ({ctx, input}) => {
-    console.log(input,'ini input dari trpc')
-    const message = await ctx.prisma.message.create({
-      data:{
-         fromUser: ctx.session.user.id,
-         fromUserName: ctx.session.user.name ?? "unknown",      
-         toUser: input.toUser,
-         message: input.message,
-         productId: input.productId
-      }
-    })
-    return message;
-  }),
+  // sendMessage: protectedProcedure
+  // .input(z.object({message: z.string(), productId: z.string(),
+  //   toUser: z.string()
+  // }))
+  // .mutation(async ({ctx, input}) => {
+  //   console.log(input,'ini input dari trpc')
+  //   const message = await ctx.prisma.message.create({
+  //     data:{
+  //        fromUser: ctx.session.user.id,
+  //        fromUserName: ctx.session.user.name ?? "unknown",      
+  //        toUser: input.toUser,
+  //        message: input.message,
+  //        productId: input.productId
+  //     }
+  //   })
+  //   return message;
+  // }),
   sendMessageToUser: protectedProcedure
   .input(z.object({message: z.string(), authorId: z.string()}))
   .mutation(async ({ctx, input}) => {
@@ -33,22 +33,87 @@ export const messageRouter = createTRPCRouter({
       if (!author) {
         throw new Error("Author not found.");
       }
-      
-      const message = await ctx.prisma.message.create({
-        data: {
-          fromUser: ctx.session.user.id, 
-          fromUserName: ctx.session.user.name ?? "Unknown",
-          toUser: input.authorId,
-          message: input.message,
+
+      let conversation = await ctx.prisma.conversation.findFirst({
+        where: {
+          conversationUsers: {
+            every: {
+              userId: {
+                in: [ctx.session.user.id, input.authorId],
+              },
+            },
+          },
         },
       });
 
+  
+      if (!conversation) {
+        conversation = await ctx.prisma.conversation.create({
+          data: {
+            conversationUsers: {
+              create: [
+                { userId: ctx.session.user.id },
+                { userId: input.authorId },
+              ],
+            },
+          },
+        });
+      }
+
+      const message = await ctx.prisma.message.create({
+        data: {
+          message: input.message,
+          user: {
+            connect: {
+              id: ctx.session.user.id,
+            },
+          },
+          conversation: {
+            connect: {
+              id: conversation?.id,
+            },
+          },
+        },
+      });
+ 
       return message.message; 
     } catch (error:any) {
       throw new Error("Error sending the message: " + error.message);
     }
   }),
-  // getMessage: protectedProcedure
-  // .input(z.object({}))
+  conversations: protectedProcedure
+  .query(({ctx}) => {
+    return ctx.prisma.conversationUser.findMany({
+      where:{
+        userId: ctx.session.user.id
+      },
+           include: {
+        conversation: {
+          include: {
+            conversationUsers: {
+              include: {
+                user: {
+                  select: {
+                    id: true,
+                    name: true,
+                    image: true,
+                    // username: true,
+                  },
+                },
+              },
+            },
+            lastMessage: true,
+          },
+        },
+      },
+      orderBy: {
+        conversation: {
+          lastMessageId: "desc",
+        },
+      },
+
+    })
+  })
+  
 
 });
