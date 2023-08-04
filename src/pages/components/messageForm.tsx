ac80da36b630d8c05bf45d63ca2feb05aace5845
MuhammadFarhanz <farhan.lagi.tidur@gmail.com>
2023-08-04 23:35:39 +0700

    fix bug

--- src/pages/components/messageForm.tsx
+++ src/pages/components/messageForm.tsx
@@ -1,43 +1,52 @@
 // MessageForm.tsx
-import React, { useEffect, useRef } from 'react';
-import { useFormik } from 'formik';
-import { api } from '~/utils/api';
-import { useMutation } from '@tanstack/react-query';
-import { useSendMessage } from '../hooks/useSendMessage';
-import { NEW_MESSAGE } from '../constants';
+import React, { useEffect, useRef } from "react";
+import { useFormik } from "formik";
+import { api } from "~/utils/api";
+import { useMutation } from "@tanstack/react-query";
+import { useSendMessage } from "../hooks/useSendMessage";
+import { NEW_MESSAGE } from "../constants";
 interface MessageFormProps {
   recipient: string | string[] | undefined;
-  conversationId: string | null;
-  setSelectedConversationId: React.Dispatch<React.SetStateAction<string | null>>;
+  currentConversationId: string | null;
+  setSelectedConversationId: React.Dispatch<
+    React.SetStateAction<string | null>
+  >;
 }
 interface Message {
   message: string;
 }
 
-const MessageForm: React.FC<MessageFormProps> = ({ recipient, conversationId , setSelectedConversationId}) => {
- const utils = api.useContext()
+const MessageForm: React.FC<MessageFormProps> = ({
+  recipient,
+  currentConversationId,
+  setSelectedConversationId,
+}) => {
+  const utils = api.useContext();
+  console.log(recipient, "ini recipient");
 
   const sendMessageMutation = api.message.sendMessage.useMutation();
 
   const formik = useFormik<Message>({
     initialValues: {
-      message: '',
+      message: "",
     },
     onSubmit: async (values: Message, { resetForm }) => {
-      const recipientId = typeof recipient === 'string' ? recipient : '';
+      const recipientId = typeof recipient === "string" ? recipient : "";
+      console.log(recipientId, "ini coyy");
       if (values) {
-        sendMessageMutation.mutate({
+        sendMessageMutation.mutate(
+          {
             messageText: values.message,
-            ...(conversationId === NEW_MESSAGE
-              ? { userId: recipientId }
-              : { conversationId: conversationId }),
+            ...(currentConversationId === NEW_MESSAGE
+              ? { userId: recipientId! }
+              : { conversationId: currentConversationId }),
           },
           {
-            onSettled: (data, error ) => {
-              if (conversationId !== NEW_MESSAGE) {
+            onSettled: (data, error) => {
+              if (currentConversationId !== NEW_MESSAGE) {
                 utils.message.conversations.invalidate();
                 utils.message.messages.invalidate({
-                  conversationId: conversationId!,
+                  conversationId: currentConversationId!,
                 });
               }
               if (data) {
@@ -46,24 +55,25 @@ const MessageForm: React.FC<MessageFormProps> = ({ recipient, conversationId , s
               if (error) {
                 alert(error.message);
               }
-         
-            }
+            },
           }
-          ),
-        console.log('bisa yok')
+        ),
+          console.log("bisa yok");
       } else {
-        console.log('id does not exist');
+        console.log("id does not exist");
       }
       resetForm();
     },
-      
   });
 
   return (
-    <form onSubmit={formik.handleSubmit}  className=" h-[10%] border-black  border-t-2 shadow-md  px-8 pt-6 pb-6 absolute bottom-0 w-full">
-      <div className="flex" >
+    <form
+      onSubmit={formik.handleSubmit}
+      className=" absolute bottom-0  h-[10%] w-full  border-t-2 border-black px-8 pb-6 pt-6 shadow-md"
+    >
+      <div className="flex">
         <input
-          className="shadow appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="focus:shadow-outline w-full appearance-none rounded-full border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
           id="message"
           name="message"
           placeholder="Enter message"
@@ -72,10 +82,21 @@ const MessageForm: React.FC<MessageFormProps> = ({ recipient, conversationId , s
         />
         <button
           type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 ml-2 rounded-full focus:outline-none focus:shadow-outline"
+          className="focus:shadow-outline ml-2 rounded-full bg-blue-500 px-2 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+            />
           </svg>
         </button>
       </div>
--- src/pages/hooks/useSendMessage.ts
+++ src/pages/hooks/useSendMessage.ts
@@ -5,11 +5,12 @@ export const useSendMessage = () => {
 //   const messages = useGetMessages(selectedConversationId);
   const sendMessageMutation = api.message.sendMessage.useMutation();
 
-  const sendMessage = async (productData:any) => {
+  const sendMessage = async (messageData:any) => {
 
     try {
-      const data = await sendMessageMutation.mutateAsync(productData.messageText.message, productData.conversationId)
+      const data = await sendMessageMutation.mutateAsync(messageData.messageText.message, messageData.conversationId)
       
+      return data;
     } catch (error) {
       // Handle error if needed
       console.error('Failed to sendMessage:', error);
