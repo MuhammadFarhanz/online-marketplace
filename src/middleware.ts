1878e25050df97675b1e59a025804797028d5491
MuhammadFarhanz <farhan.lagi.tidur@gmail.com>
2023-08-16 00:05:10 +0700

    try another middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,22 @@
-import { NextResponse } from 'next/server'
-import { getToken } from 'next-auth/jwt'
-import { NextApiRequest, NextApiResponse } from 'next'
+// import { NextResponse } from 'next/server'
+// import { getToken } from 'next-auth/jwt'
+// import { NextApiRequest, NextApiResponse } from 'next'
 
-// This function can be marked `async` if using `await` inside
-export async function middleware(req: NextApiRequest, res: NextApiResponse) {
+// // This function can be marked `async` if using `await` inside
+// export async function middleware(req: NextApiRequest, res: NextApiResponse) {
 
-  // const session = await getServerSession(authOptions)
-  const session = await getToken({req})
+//   // const session = await getServerSession(authOptions)
+//   const session = await getToken({req})
   
-  if(!session){
-    // return NextResponse.redirect('/features/sign-in')
-    return NextResponse.redirect(new URL('/api/auth/signin', req.url))
-  } 
-}
+//   console.log(session,'cok')
+//   if(!session){
+//     // return NextResponse.redirect('/features/sign-in')
+//     return NextResponse.redirect(new URL('/api/auth/signin', req.url))
+//   } 
+// }
  
+
+export { default } from "next-auth/middleware"
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: ['/product/:path*','/chat','/dashboard'],
